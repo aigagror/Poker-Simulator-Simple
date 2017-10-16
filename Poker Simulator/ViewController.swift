483ca6452b1eaca8795fe334cfe2346982032589
Eddie Huang <aigagror@gmail.com>
2017-10-15 22:13:38 -0500

    First commit

---
+++ Poker Simulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poker Simulator
+//
+//  Created by Edward Huang on 10/15/17.
+//  Copyright © 2017 Eddie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
