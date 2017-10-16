f12f74d0a1fa28c9d98dd49774a85a5e643e53e3
Eddie Huang <aigagror@gmail.com>
2017-10-15 22:39:50 -0500

    Started UI storyboard

--- Poker Simulator/SimulatorViewController.swift
+++ Poker Simulator/SimulatorViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class SimulatorViewController: UIViewController {
-
+    @IBOutlet var communityCardImageViews: [UIImageView]!
+    
+    @IBOutlet var playerCardImageViews: [UIImageView]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for 
+        
     }
 
     override func didReceiveMemoryWarning() {
